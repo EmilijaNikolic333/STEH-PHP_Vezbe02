20153612b87849640a47ed224c3129c455b86f60
EmilijaNikolic333 <nikolicemilija03@gmail.com>
2025-06-09 03:02:36 +0200

    Implementiran model za Usera i login funkcionalnost

---
+++ model/user.php
@@ -0,0 +1,27 @@
+<?php
+
+class User{
+    public $id;
+    public $username;
+    public $password;
+
+    public function __construct($id, $username, $pass) {
+        $this->id = $id;
+        $this->username = $username;
+        $this->password = $pass;
+    }
+
+    public function loginUser($username, $password, $conn){
+        $queryStr = "SELECT * FROM user WHERE username = '$username' AND password = '$password'";
+        //return true;
+        //poslati upit nad bazom
+        return $conn->query($queryStr);
+
+    }
+
+
+}
+
+
+
+?>
